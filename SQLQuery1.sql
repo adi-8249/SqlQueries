107068c37704e0a86a8145471decbbea12e78936
Anjaneya <anjaneyaprasad9437@gmail.com>
2022-03-10 01:43:10 +0530

    Added:Sql Clauses

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -56,3 +56,37 @@ insert into Department(dept_name,dept_strength,id) values ('Etc',120,102);
 insert into Department(dept_name,dept_strength,id) values ('EEE',120,103);
 
 select * from Department;
+
+
+/*Clauses*/
+
+create table Database1
+(
+d_id int not null,
+d_name varchar(40) unique,
+d_age int,
+d_sal int not null,
+d_dept varchar(30) 
+);
+
+select * from Database1;
+
+insert into Database1(d_id,d_name,d_age,d_sal,d_dept) values (101,'Jugal',22,10000,'mech');
+insert into Database1(d_id,d_name,d_age,d_sal,d_dept) values (102,'Bidesi',29,20000,'EEE');
+insert into Database1(d_id,d_name,d_age,d_sal,d_dept) values (103,'Srikant',32,30000,'ETC');
+insert into Database1(d_id,d_name,d_age,d_sal,d_dept) values (104,'Sairam',20,30000,'mech');
+insert into Database1(d_id,d_name,d_age,d_sal,d_dept) values (105,'Srinibas',33,20000,'EEE');
+
+select * from Database1 where d_age>25;
+
+SELECT d_age ,count(d_id)
+from Database1
+GROUP BY d_age
+having count(d_sal)>15000;
+
+select * from Database1 order by d_name asc;
+select * from Database1 order by d_name desc;
+
+select top 3 d_name,d_dept from Database1
+order by d_sal;
+
