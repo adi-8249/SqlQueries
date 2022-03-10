9dc9f2f508d076572cb41872d75b16b492103ecb
Anjaneya <anjaneyaprasad9437@gmail.com>
2022-03-10 21:09:04 +0530

    Added:SubQueries

--- SQLQuery1.sql
+++ SQLQuery1.sql
@@ -8,7 +8,7 @@ create table EmployeeTable
  emp_sal int not null,
  emp_startdate datetime
 );
-drop table EmployeeTable;
+
 alter table EmployeeTable add emp_designation varchar(40) not null;
 truncate table EmployeeTable;
 /* DQL command*/
@@ -46,7 +46,7 @@ insert into Bridgelabz(id,name,age) values (103,'Kriti',26);
 select * from Bridgelabz;
 
 create table Department
-(
+(                                        -------foreign key----------
 dept_name varchar(40),
 dept_strength int not null,
 id int foreign key references Bridgelabz(id)
@@ -56,7 +56,7 @@ insert into Department(dept_name,dept_strength,id) values ('Etc',120,102);
 insert into Department(dept_name,dept_strength,id) values ('EEE',120,103);
 
 select * from Department;
-
+select * from Bridgelabz;
 
 /*Clauses*/
 
@@ -77,16 +77,45 @@ insert into Database1(d_id,d_name,d_age,d_sal,d_dept) values (103,'Srikant',32,3
 insert into Database1(d_id,d_name,d_age,d_sal,d_dept) values (104,'Sairam',20,30000,'mech');
 insert into Database1(d_id,d_name,d_age,d_sal,d_dept) values (105,'Srinibas',33,20000,'EEE');
 
-select * from Database1 where d_age>25;
+select * from Database1 where d_age>25;----where constraints-----
 
-SELECT d_age ,count(d_id)
-from Database1
-GROUP BY d_age
-having count(d_sal)>15000;
+ -------group by-------
+ /* here it will show two mech column because we are grouping sal and dept which is not same */
+ select d_sal,d_dept, sum(d_age) as [total_Age] 
+ from Database1 group by d_sal,d_dept;
 
-select * from Database1 order by d_name asc;
+select d_dept,sum(d_sal) as total_salary 
+from Database1 group by d_dept;
+
+------assending and descending-------
+select * from Database1 order by d_name asc;      
 select * from Database1 order by d_name desc;
 
+-------top--------
 select top 3 d_name,d_dept from Database1
 order by d_sal;
 
+
+/*subqueries*/
+
+create table Product
+(
+   p_id int not null,
+   p_name varchar(30) unique not null,
+   p_price int,
+   p_availability varchar(30),
+)
+insert into Product(p_id,p_name,p_price,p_availability) values (1,'Herohonda',50000,'Cuttack');
+insert into Product(p_id,p_name,p_price,p_availability) values (2,'Discover',60000,'Cuttack');
+insert into Product(p_id,p_name,p_price,p_availability) values (3,'Pulsur',50000,'Bhubaneswar');
+insert into Product(p_id,p_name,p_price,p_availability) values (4,'Activa',40000,'Cuttack');
+
+select * from Product;
+
+
+----give me the product name whose id is 2-----
+select p_name from Product where p_availability in (select p_availability from Product where p_id=2);
+
+select p_availability from Product where p_price in (select p_price from Product where p_name='Activa');
+
+select p_price from Product where p_id in(select p_id from Product where p_name='Discover');
